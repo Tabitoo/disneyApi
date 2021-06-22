cc1043b73fb8b5af18d2a50b78c2caaf9ffa7549
Tabitoo <lautarovaldez4@gmail.com>
2021-06-22 12:47:24 -0300

    modificaciones en el archivo env

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
-
 module.exports = {
   "development": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
-    "database": "disneymovies",
+    "database": process.env.DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
